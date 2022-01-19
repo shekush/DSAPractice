c7b6b3f30493137e690d27b50e91a76121b3d109
Shejal <shejal270999@gmail.com>
2022-01-19 15:21:22 +0530

    added code for Breadth First Traversal

--- graphs/src/graphs/BFTraversal.java
+++ graphs/src/graphs/BFTraversal.java
@@ -1,10 +1,37 @@
 package graphs;
+import java.util.*;
 
 public class BFTraversal {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	//time complexity is O(N+E) and Space Complexity is O(N+E)+O(N)+O(N)
+	
+	public ArrayList<Integer> bft(ArrayList<ArrayList<Integer>> adj, int v)
+	{
+		ArrayList<Integer> bfs = new ArrayList<>();
+		boolean vis[] = new boolean[v+1];
+		for(int i=1; i<=v; i++)
+		{
+			if(vis[i]==false)
+			{
+				Queue<Integer> q = new LinkedList<>();
+				q.add(i);
+				vis[i]=true;
+				while(!q.isEmpty())
+				{
+					Integer tmp = q.poll();
+					bfs.add(tmp);
+					for(Integer item:adj.get(tmp))
+					{
+						if(vis[item]==false)
+						{
+							vis[item]=true;
+							bfs.add(item);
+						}
+					}
+				}
+			}
+		}
+		
+		return bfs;
 	}
-
 }
