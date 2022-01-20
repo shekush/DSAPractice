5456a10041c9360e6ec279c335a098da074369b2
Shejal <shejal270999@gmail.com>
2022-01-20 22:30:43 +0530

    Topological Sort using Depth First Traversal

---
+++ graphs/src/graphs/TopologicalDFS.java
@@ -0,0 +1,38 @@
+package graphs;
+import java.util.*;
+
+public class TopologicalDFS {
+	
+	public void topo(int tmp, Stack<Integer> s, boolean[] vis, ArrayList<ArrayList<Integer>> adj)
+	{
+		vis[tmp] = true;
+		for(Integer i: adj.get(tmp))
+		{
+			if(vis[i]==false)
+				topo(i,s,vis,adj);
+		}
+		s.push(tmp);
+	}
+	
+	public int[] topo(int v, ArrayList<ArrayList<Integer>> adj)
+	{
+		Stack<Integer> s = new Stack<>();
+		boolean vis[] = new boolean[v+1];
+		for(int i=1; i<=v; i++)
+		{
+			if(vis[i]==false)
+			{
+				topo(i,s,vis,adj);
+			}
+		}
+		int res[] = new int[v];
+		int i=0;
+		while(!s.isEmpty())
+		{
+			res[i++]=s.pop();
+		}
+		
+		return res;
+	}
+
+}
