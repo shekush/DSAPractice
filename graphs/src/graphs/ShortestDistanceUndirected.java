022914aba1afc4ecfb1ac5fddde4b8786b8dfe47
Shejal <shejal270999@gmail.com>
2022-01-22 19:35:40 +0530

    Shortest Distance in an Undirected Graph

---
+++ graphs/src/graphs/ShortestDistanceUndirected.java
@@ -0,0 +1,35 @@
+package graphs;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class ShortestDistanceUndirected {
+	
+	//time complexity is O(N+E)
+	
+	public int[] shortest(ArrayList<ArrayList<Integer>> adj, int v, int src)
+	{
+		int dis[] = new int[v+1];
+		Queue<Integer> q = new LinkedList<>();
+		dis[src] = 0;
+		q.add(src);
+		while(!q.isEmpty())
+		{
+			Integer tmp = q.poll();
+			for(Integer item:adj.get(tmp))
+			{
+				if(item!=src)
+				{
+					if((dis[tmp]+1 < dis[item])&& dis[item]!=0)
+					{
+						dis[item]=dis[tmp]+1;
+						q.add(item);
+					}
+				}
+			}
+		}
+		return dis;
+	}
+
+}
