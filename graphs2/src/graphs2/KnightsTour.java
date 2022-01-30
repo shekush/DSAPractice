ef0b588f23ba984e4717b0d75cf8655a7491eb21
Shejal <shejal270999@gmail.com>
2022-01-30 22:23:09 +0530

    Added Knights Tour

--- graphs2/src/graphs2/KnightsTour.java
+++ graphs2/src/graphs2/KnightsTour.java
@@ -7,46 +7,70 @@ import java.io.InputStreamReader;
 public class KnightsTour
 {
 	
-	public void printBoard(int i, int j, int m, int n, int arr[][], int move)
+	public void printArray(int arr[][],int n)
 	{
-		if(i<0 || j<0 || i>=m || j>=n || )
+		System.out.println("for printing");
+		for(int i=0; i<n; i++)
+		{
+			for(int j=0; j<n; j++)
+			{
+				System.out.print(arr[i][j]+"  ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+		System.out.println();
+	}
+	
+	public void printBoard(int i, int j, int n, int arr[][], int move)
+	{
+		System.out.println("went in method");
+		
+		if(i<0 || j<0 || i>=n || j>=n || arr[i][j]>0)
+		{
+			System.out.println("returned");
+			return;
+		}
+			
+		else if(move==n*n)
+		{
+			arr[i][j] = move;
+			printArray(arr,n);
+			arr[i][j]=0;
+			System.out.println("in else");
+			return;
+		}
 		
 		arr[i][j]=move;
+		System.out.println("value of move  "+move);
 		
-		printBoard(i-2,j-1,m,n,arr,move+1);
-		printBoard(i-2,j+1,m,n,arr,move+1);
-		printBoard(i+2,j-1,m,n,arr,move+1);
-		printBoard(i+2,j+1,m,n,arr,move+1);
-		printBoard(i-1,j-2,m,n,arr,move+1);
-		printBoard(i-1,j+2,m,n,arr,move+1);
-		printBoard(i,j,m,n,arr,move+1);
-		printBoard(i,j,m,n,arr,move+1);
-		
+		printBoard(i-2,j+1,n,arr,move+1);
+		printBoard(i-1,j+2,n,arr,move+1);
+		printBoard(i+1,j+2,n,arr,move+1);
+		printBoard(i+2,j+1,n,arr,move+1);
+		printBoard(i+2,j-1,n,arr,move+1);
+		printBoard(i+1,j-2,n,arr,move+1);
+		printBoard(i-1,j-2,n,arr,move+1);
+		printBoard(i-2,j-1,n,arr,move+1);
+			
 		arr[i][j]=0;
+		
+		System.out.println("end of method");
 	}
 
 	public static void main(String[] args)throws IOException
 	{
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		System.out.println("Enter the value of m and n: ");
-		int m = Integer.parseInt(in.readLine());
+		System.out.println("Enter the dimension n: ");
 		int n = Integer.parseInt(in.readLine());
 		
-		int arr[][] = new int[m][n];
-		System.out.println("Enter the elements: ");
-		for(int i=0; i<m; i++)
-		{
-			String inp[] = in.readLine().trim().split(" ");
-			for(int j=0; j<n; j++)
-			{
-				arr[i][j] = Integer.parseInt(inp[j]);
-			}
-		}
+		int arr[][] = new int[n][n];
 		System.out.println("Enter the Row and Column of starting point: ");
 		int i = Integer.parseInt(in.readLine());
 		int j = Integer.parseInt(in.readLine());
 		
 		KnightsTour obj = new KnightsTour();
-		obj.printBoard(i,j,m,n,arr, 1);
+		System.out.println("method called");
+		obj.printBoard(i,j,n,arr, 1);
 	}
 }
