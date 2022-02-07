0baa0fb0a3cc78e8d032d4c3747e2f7d6707cc35
Shejal <shejal270999@gmail.com>
2022-02-07 12:13:48 +0530

    Started Khan's Algorithm

---
+++ graphs2/src/graphs2/Khans.java
@@ -0,0 +1,11 @@
+package graphs2;
+
+public class Khans 
+{
+	//this algorithm is an iterative way to write topological sort
+	//topological sort is valid only for acyclic directed graph
+	
+	
+	
+
+}
