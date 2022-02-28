83ad3fb8b820d67e5a2707ea4eda600666350248
Shejal <shejal270999@gmail.com>
2022-02-28 16:41:19 +0530

    Started Josephus Problem

---
+++ Bit Manipulatoin/src/JosephusProblem.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class JosephusProblem 
+{
+	public static int calcPower(int n)
+	{
+		
+	}
+	
+	public static int solve(int n)
+	{
+		
+	}
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number");
+		int n = sc.nextInt();
+		
+		int ans = solve(n);
+		
+		System.out.println(ans);
+	}
+}
