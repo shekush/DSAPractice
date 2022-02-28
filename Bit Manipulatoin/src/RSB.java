5cbfb4b74fc78b2a3eebb96e3106e3bef9556452
Shejal <shejal270999@gmail.com>
2022-02-28 16:15:07 +0530

    Added RSB

---
+++ Bit Manipulatoin/src/RSB.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class RSB {
+
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number: ");
+		int n = sc.nextInt();
+		int rsb = n & -n;
+		System.out.println(rsb);
+	}
+}
