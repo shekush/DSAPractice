c32f0f4a655dcf723a8b02c593222c39ed48f598
Shejal <shejal270999@gmail.com>
2022-03-29 22:33:28 +0530

    Added SOduku SOlver

---
+++ Bit Manipulatoin/src/Soduku.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Soduku 
+{
+
+	public static void main(String[] args) throws IOException 
+	{
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Enter ")
+
+	}
+
+}
