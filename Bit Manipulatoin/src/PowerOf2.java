904a6a0a65a743cf8e55b55ff10c5904afd07990
Shejal <shejal270999@gmail.com>
2022-03-30 17:35:56 +0530

    added Power of 2 checking

---
+++ Bit Manipulatoin/src/PowerOf2.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class PowerOf2 
+{
+	public static void main(String[] args) throws IOException
+	{
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Enter the number: ");
+		int a=Integer.parseInt(in.readLine());
+		int res = (a & (a-1));
+		if(res==0)
+			System.out.println("True");
+		else
+			System.out.println("False");
+	}
+
+}
---
+++ Bit Manipulatoin/src/PowerOf4.java
@@ -0,0 +1,9 @@
+
+public class PowerOf4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
