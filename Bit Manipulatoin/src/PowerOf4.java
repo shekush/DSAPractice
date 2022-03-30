91457474dc184280dbda609081d8142ee47aaf6b
Shejal <shejal270999@gmail.com>
2022-03-30 17:46:37 +0530

    added Power of 4 checking

--- Bit Manipulatoin/src/PowerOf4.java
+++ Bit Manipulatoin/src/PowerOf4.java
@@ -1,8 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class PowerOf4 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException
+	{
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Enter the number: ");
+		int a=Integer.parseInt(in.readLine());
+		int fac = 0b01010101010101010101010101010101;
+		int res = (a & (a-1));
+		if(res==0 && a>0 && (fac&a)>0)
+			System.out.println("True");
+		else
+			System.out.println("False");
 
 	}
 
