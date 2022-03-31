c65c215d882e06221a438a44311c419068354f12
Shejal <shejal270999@gmail.com>
2022-03-31 22:02:25 +0530

    added Nth Palindrome Binary

--- Bit Manipulatoin/src/NthBinaryPalindrome.java
+++ Bit Manipulatoin/src/NthBinaryPalindrome.java
@@ -4,12 +4,47 @@ import java.io.InputStreamReader;
 
 public class NthBinaryPalindrome 
 {
-
+	public static int getRev(int n)
+	{
+		int rev=0;
+		while(n!=0)
+		{
+			int lowestBit = (n & 1);
+			rev|=lowestBit;
+			rev<<=1;
+			n>>=1;
+		}
+		return rev;
+	}
+	public static int NthPalin(int n)
+	{
+		int count=1;
+		int len=1;
+		while(count<n)
+		{
+			len++;
+			int elementsThisLength = (1 << (len-1)/2);
+			count+=elementsThisLength;
+		}
+		count-=(1 << (len-1)/2);
+		
+		int distanceFromLastGroup = n-count-1;
+		int ans = (1 << (len-1));
+		ans |= (distanceFromLastGroup << (len/2));
+		
+		int toBeReversed = (ans >> (len/2));
+		int rev = getRev(toBeReversed);
+		
+		ans|=rev;
+		return ans;
+	}
 	public static void main(String[] args) throws IOException
 	{
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		System.out.println("Enter the value of N: ");
 		int n = Integer.parseInt(in.readLine().trim());
+		int palindrome = NthPalin(n);
+		System.out.println(palindrome);
 	}
 
 }
