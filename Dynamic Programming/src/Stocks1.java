c7b92d3301bec83a9f309f8c43f7ad911d71acb3
Shejal <shejal270999@gmail.com>
2022-05-28 22:23:31 +0530

    Added Stocks1 Problem

---
+++ Dynamic Programming/src/Stocks1.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Stocks1 {
+
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number of stocks:");
+		int n = sc.nextInt();
+		int prices[] = new int[n];
+		System.out.println("Enter the prices:");
+		for(int i=0; i<n; i++)
+		{
+			prices[i]=sc.nextInt();
+		}
+		
+		int minPrice = Integer.MAX_VALUE;
+		int overallProfit = Integer.MAX_VALUE, currentProfit;
+		
+		for(int i=0; i<n; i++)
+		{
+			if(prices[i]<minPrice)
+				minPrice = prices[i];
+			
+			currentProfit = prices[i] - minPrice;
+			if(currentProfit<overallProfit)
+				overallProfit = currentProfit;
+		}
+		
+		System.out.println(overallProfit);
+		sc.close();
+	}
+
+}
